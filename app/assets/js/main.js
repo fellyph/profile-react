1b40467104e54cab150abebdd66e4d09cc2400ab
fellyph <fellyph.cintra@gmail.com>
2017-08-23 01:07:48 +0100

    setup webpack to load sass and ecma2015

---
+++ app/assets/js/main.js
@@ -0,0 +1,6 @@
+'use strict'
+
+import {render} from 'react-dom'
+import React from 'react'
+import App from './components'
+import css from '../scss/main.scss'
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,22 @@
 module.exports = {
+  devtool: 'source-map',
   entry: './app/assets/js/main.js',
   output: {
     path: __dirname + '/build',
     publicPath: '/build/',
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {test: /\.scss$/, exclude: /nodule_modules/, use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader" // translates CSS into CommonJS
+        }, {
+          loader: "sass-loader" // compiles Sass to CSS
+        }]
+      },
+      {test: /\.js$/, exclude: /nodule_modules/, use: 'babel-loader'}
+    ]
   }
 }
